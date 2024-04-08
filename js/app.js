17600e297e26f885202de7e1881aa88aa5bf329b
francescomatteucci <francesco.matteucci.boolean@gmail.com>
2024-04-08 16:36:10 +0200

    ADD: ul control

--- js/app.js
+++ js/app.js
@@ -1,34 +1,30 @@
 const { createApp } = Vue
 
-  createApp({
+createApp({
     data() {
-      return {
-        message: 'Hello Vue!',
-        email: null,
-        emails: [],
-      }
+        return {
+            message: 'Hello Vue!',
+            email: null,
+            emails: [],
+        }
     },
     methods: {
         generateEmail(n) {
-            for(let i = 0; i < n; i++){
-            axios
-        .get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((response) =>{
-            // console.log(response.data.response)
-                this.email = response.data.response
-                this.emails.push(this.email)
-                
+            for (let i = 0; i < n; i++) {
+                axios
+                    .get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then((response) => {
+                        // console.log(response.data.response)
+                        this.email = response.data.response
+                        this.emails.push(this.email)
 
-            
-   
-        })
+
+
+
+                    })
+            }
         }
-    }
     },
-    created(){
-        this.generateEmail()
-        
-    },
-    
-  }).mount('#app')
-  
+
+
+}).mount('#app')
